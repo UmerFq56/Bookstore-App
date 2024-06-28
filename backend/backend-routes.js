0199ddd0eaed3be3981efd3a22e98dc188b288cf
umer <umer.farooqui135@gmail.com>
2024-06-28 00:59:32 -0400

    created controllers adn finished backend

--- backend/backend-routes.js
+++ backend/backend-routes.js
@@ -1,35 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('./schema');
+const {createBook, getBooks, getSingleBook, deleteBook, updateBook} = require('./bookController')
 
-router.get('/' , (req, res) => {
-        res.send('Get All Books')
-    })    
+router.get('/' , getBooks)    
 
-router.get('/:id' , (req,res) => {
-    res.json({mssg: 'GET a  book'})
-})
+router.get('/:id', getSingleBook)
 
-router.post('/', async (req,res) => {
-    const {title, author, rating, pages, genres, review} = req.body
+router.post('/', createBook)
 
-    try{ 
-            const book = await Book.create({title, author, rating, pages, genres, review})
-            res.status(200).json(book)
+router.delete('/:id', deleteBook)
 
-    } catch (error) {
-        res.status(400).json({error: error.message })
-    }
-    
-})
-
-router.delete('/:id', (req,res) => {
-    res.json({mssg: 'DELETE a  book'})
-})
-
-router.patch('/:id', (req,res) => {
-    res.json({mssg: 'UPDATE a book'})
-})
+router.patch('/:id', updateBook)
 
 
 module.exports = router;
---
+++ backend/bookController.js
@@ -0,0 +1,87 @@
+ const Book = require('./schema')
+ const mongoose = require('mongoose')
+
+
+ //get all books
+
+ const getBooks = async (req, res) => {
+    const books = await Book.find({}).sort({createdAt: -1})
+    res.status(200).json(books)
+ }
+
+
+ //get a single book
+
+ const getSingleBook = async (req,res) => {
+    const {id} = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book found'})
+    }
+
+    const singleBook = await Book.findById(id)
+
+    if (!singleBook) {
+        res.status(404).json({error: 'No such book found'})
+    }
+    res.status(200).json(singleBook)
+ }
+
+
+ // create a new Book
+ const createBook = async (req, res) => {
+    const {title, author, rating, pages, genres, review} = req.body
+
+    try{ 
+            const book = await Book.create({title, author, rating, pages, genres, review})
+            res.status(200).json(book)
+
+    } catch (error) {
+        res.status(400).json({error: error.message })
+    }
+ }
+
+ const deleteBook = async (req,res) => {
+    const {id} = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book found to be deleted'})
+    }
+
+    const singleBook = await Book.findOneAndDelete({_id: id})
+
+    if (!singleBook) {
+        res.status(404).json({error: 'No such book found'})
+    }
+    res.status(200).json(singleBook)
+
+ }
+
+ // update a book
+
+ const updateBook = async (req,res) => {
+    const {id} = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book found'})
+    }
+
+    const singleBook = await Book.findByIdAndUpdate({_id:id}, {...req.body})
+
+    if (!singleBook) {
+        res.status(404).json({error: 'No such book found'})
+    }
+    res.status(200).json(singleBook)
+ }
+
+
+
+
+
+ module.exports = {
+    createBook,
+    getBooks,
+    getSingleBook,
+    updateBook,
+    deleteBook
+ }
